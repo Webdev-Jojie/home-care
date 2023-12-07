62f228aee24b6c7b77c7c0bdf828c0d01446a65d
JOJIE <149245077+Webdev-Jojie@users.noreply.github.com>
2023-12-08 00:52:41 +0800

    Third-Commit-ResponsivePage1

--- Webdev/main.js
+++ Webdev/main.js
@@ -2,3 +2,24 @@ function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
   }
 
+  function openNav() {
+    const overlay = document.querySelector('.overlay');
+    const navContainer = document.querySelector('.nav-container');
+    
+    overlay.style.display = 'block';
+    navContainer.style.display = 'block';
+    setTimeout(() => {
+        navContainer.classList.add('active');
+    }, 10);
+}
+
+function closeNav() {
+    const overlay = document.querySelector('.overlay');
+    const navContainer = document.querySelector('.nav-container');
+
+    navContainer.classList.remove('active');
+    setTimeout(() => {
+        overlay.style.display = 'none';
+        navContainer.style.display = 'none';
+    }, 300);
+}
